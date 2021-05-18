51cae6b115f57cff8e08f22b890f14f2afd985a9
Rusu Traian Cristian <rusutraiancristian@gmail.com>
2021-05-18 16:34:47 +0200

    latest changes TOML

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use helpers::create::*;
 use helpers::calculate::*;
 
 fn main() {
-   
-
     let mata = Cash::create_cash("usd", 12000).add(500).subtract(2200).multiply(3).subtract(900).divide(2);
     println!("you have {} {}", mata.get_amount(), mata.get_currency());
 }
